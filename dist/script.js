8c7136039a6d3c058361760faf4daa91a5517406
Daniil <uskov.danya@gmail.com>
2022-09-21 05:54:32 +0300

    second day

--- dist/script.js
+++ dist/script.js
@@ -1,3 +1,8 @@
+/******/ (function() { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
 const sliderMain = new Swiper('.slider_main', {
   mousewheel: true,
   slidesPerView: 2.5,
@@ -9,8 +14,9 @@ const sliderMain = new Swiper('.slider_main', {
     680: {
       slidesPerView: 2.5,
       spaceBetween: 2
-
     }
   }
 });
-
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
